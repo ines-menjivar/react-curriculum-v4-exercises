8fd8ef70e57b061d3e703469ad8fa7f10937eb2e
Ines Lopez <ines.menjivar1996@gmail.com>
2026-07-11 15:17:30 -0500

    week 1 exercise

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -2,11 +2,23 @@
 //Exercise: Build an "About Me" Component in this file
 
 export default function StudentWork() {
-  //add variables here
+  const name = 'Ines';
+  const age = 29;
+  const hobbies = ['Coding', 'Reading', 'Pugs', 'Music'];
+
   return (
     <div>
-      {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About Me</h1>
+      <p>
+        Hi! My name is {name}. I am {age} years old and excited to learn React!
+      </p>
+
+      <h2>My Hobbies</h2>
+      <ul>
+        {hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
